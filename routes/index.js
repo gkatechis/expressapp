336af8ed21dcca6618c86cbe663b0e89e0aac875
Greg K <gkatechis@zendesk.com>
2021-09-15 16:19:02 -0500

    commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/sidebar', function (req, res, next) {
+    res.render('sidebar', {
+      title: 'Sidebar app'
+    })
+  }
+);
+
+module.exports = router;
